7ff03e7ae868fbc5242aa6a0aab961532011a293
Zeeshan Work LP <bscs15078@itu.edu.pk>
2020-04-04 04:44:10 +0500

    testing code added

---
+++ firmware/test/read_pulse_in/read_pulse_in.ino
@@ -0,0 +1,17 @@
+void setup() {
+  Serial.begin(9600);
+
+  while (!Serial) delay(25);
+
+  Serial.println("Start!");
+
+  pinMode(D5, INPUT);
+}
+
+void loop() {
+  // https://www.arduino.cc/reference/en/language/functions/advanced-io/pulsein/
+  long duration = pulseIn(D5, HIGH, 2000000);
+  int value = duration / 20;
+
+  Serial.println(value);
+}
